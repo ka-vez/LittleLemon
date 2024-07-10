d5cc84b243b2fb242696e626a3cc557b8103029e
David <110306469+Kavez21@users.noreply.github.com>
2024-07-10 18:17:32 +0100

    Updated the models.py

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 def home(request):
     return HttpResponse('Hello David')
+
+@api_view()
+def items(request):
+    return Response({"message" : "What's Up People"})
+
