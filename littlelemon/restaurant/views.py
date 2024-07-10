34263ce68c4b85c828d7515927683b558f35f066
David <110306469+Kavez21@users.noreply.github.com>
2024-07-10 20:27:54 +0100

    Added Token Authentication

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -5,20 +5,22 @@ from rest_framework.response import Response
 from rest_framework import generics
 from .serializers import MenuSerializer, BookingSerializer
 from .models import Booking, Menu
-from rest_framework import viewsets
-
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
-
+    
 class SingleItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookingView(generics.ListCreateAPIView):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
     
 
